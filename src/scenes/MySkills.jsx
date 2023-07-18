2fe0ec3c31f82f76e093a9ae83325e9e981376be
Francis Galicia <galiciafrancis108@yahoo.com>
2023-07-18 19:10:06 -0400

    Added tech stack on the Skills And Experience Section. Also, added an experience timeline.

--- src/scenes/MySkills.jsx
+++ src/scenes/MySkills.jsx
@@ -1,10 +1,44 @@
 import LineGradient from "../components/LineGradient";
-import useMediaQuery from "../hooks/useMediaQuery";
+// import useMediaQuery from "../hooks/useMediaQuery";
 import { motion } from "framer-motion";
-// import SkillsPic from "../assets/skills-image.png";
+import tailwind from "../assets/tailwind.png";
+import git from "../assets/git.png";
+import figma from "../assets/figma.png";
+import javascript from "../assets/javascript.png";
+import typescript from "../assets/typescript.png";
+import golang from "../assets/golang.png";
+import graphql from "../assets/graphql.png";
+import react from "../assets/react.png";
+import redux from "../assets/redux.png";
+import mui from "../assets/mui.png";
+import python from "../assets/python.png";
+import node from "../assets/node.png";
+import aws from "../assets/aws.png";
+import html from "../assets/html.png";
+import mongo from "../assets/mongo.png";
+import java from "../assets/java.png";
 
 const MySkills = () => {
-  const isAboveLarge = useMediaQuery("(min-width: 1060px)");
+  // const isAboveLarge = useMediaQuery("(min-width: 1060px)");
+
+  const skillImages = [
+    { img: react, name: "React" },
+    { img: redux, name: "Redux" },
+    { img: tailwind, name: "Tailwind" },
+    { img: javascript, name: "Javascript" },
+    { img: typescript, name: "Typescript" },
+    { img: mui, name: "Material UI" },
+    { img: graphql, name: "Graphql" },
+    { img: git, name: "Git" },
+    { img: figma, name: "Figma" },
+    { img: golang, name: "Golang" },
+    { img: python, name: "Python" },
+    { img: node, name: "Node" },
+    { img: aws, name: "AWS" },
+    { img: html, name: "HTML" },
+    { img: mongo, name: "Mongo" },
+    { img: java, name: "Java" },
+  ];
 
   return (
     <section id="skills" className="pt-10 pb-24">
@@ -32,119 +66,84 @@ const MySkills = () => {
           <div className="flex justify-center">
             <LineGradient width="w-2/4" />
           </div>
-
-          {/* 
-          <p className="mt-10 mb-7">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
-            officiis explicabo dolores nemo.
-          </p> */}
         </motion.div>
-
-        {/* <div className="mt-16 md:mt-0">
-          {isAboveLarge ? (
-            <div
-              className="relative z-0 ml-20 before:absolute before:-top-10 before:-left-10
-            before:w-full before:h-full before:border-2 before:border-blue before:z-[-1]"
-            >
-              <img alt="skills" className="z-10" src={SkillsPic} />
-            </div>
-          ) : (
-            <img alt="skills" className="z-10" src={SkillsPic} />
-          )}
-        </div> */}
       </div>
 
-      {/* SKILLS */}
-      <div className="md:flex md:justify-between mt-16 gap-32">
-        {/* EXPERIENCE */}
-        <motion.div
-          className="md:w-1/3 mt-10"
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true, amount: 0.5 }}
-          transition={{ duration: 0.5 }}
-          variants={{
-            hidden: { opacity: 0, y: 50 },
-            visible: {
-              opacity: 1,
-              y: 0,
-            },
-          }}
-        >
-          <div className="relative h-32">
-            <div className="z-10">
-              <p className="font-playfair font-semibold text-5xl">01</p>
-              <p className="font-playfair font-semibold text-3xl mt-3">
-                Experience
-              </p>
-              <div className="w-1/2 md:w-3/4 h-32 bg-blue absolute right-0 top-0 z-[-1]" />
-            </div>
-          </div>
-          <p className="mt-5">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
-            cumque veritatis perferendis sapiente sunt dicta veniam.
-          </p>
-        </motion.div>
+      {/* SKILLS AND EXPERIENCE */}
+      <div className="md:flex md:justify-center mt-24 gap-24">
+        <div className="grid grid-cols-3 sm:grid-cols-4 gap-x-8 gap-y-10 text-slate-200">
+          {skillImages.map((el, idx) => {
+            console.log(el.bgColor);
+            return (
+              <div
+                key={idx}
+                className="flex flex-col justify-center items-center"
+              >
+                <div
+                  className={`w-[92px] h-[92px] flex flex-col justify-center items-center rounded-full bg-[#fef1cd]`}
+                >
+                  <img className="w-[64px]" src={el.img} alt="tailwind" />
+                </div>
+                <p className="text-center mt-2 ">{el.name}</p>
+              </div>
+            );
+          })}
+        </div>
 
-        {/* INNOVATIVE */}
-        <motion.div
-          className="md:w-1/3 mt-10"
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true, amount: 0.5 }}
-          transition={{ delay: 0.2, duration: 0.5 }}
-          variants={{
-            hidden: { opacity: 0, y: 50 },
-            visible: {
-              opacity: 1,
-              y: 0,
-            },
-          }}
-        >
-          <div className="relative h-32">
-            <div className="z-10">
-              <p className="font-playfair font-semibold text-5xl">02</p>
-              <p className="font-playfair font-semibold text-3xl mt-3">
-                Innovative
-              </p>
-              <div className="w-1/2 md:w-3/4 h-32 bg-red absolute right-0 top-0 z-[-1]" />
-            </div>
-          </div>
-          <p className="mt-5">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
-            cumque veritatis perferendis sapiente sunt dicta veniam.
-          </p>
-        </motion.div>
+        {/* Experience Timeline */}
+        <div className="relative flex flex-col gap-y-[72px] sm:mt-0 mt-[72px] text-slate-200 -z-20">
+          <div
+            className={
+              "h-0.5 w-[400px] bg-gradient-rainblue absolute rotate-90 top-[191px] -left-[182px] -z-10"
+            }
+          />
 
-        {/* IMAGINATIVE */}
-        <motion.div
-          className="md:w-1/3 mt-10"
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true, amount: 0.5 }}
-          transition={{ delay: 0.4, duration: 0.5 }}
-          variants={{
-            hidden: { opacity: 0, y: 50 },
-            visible: {
-              opacity: 1,
-              y: 0,
-            },
-          }}
-        >
-          <div className="relative h-32">
-            <div className="z-10">
-              <p className="font-playfair font-semibold text-5xl">03</p>
-              <p className="font-playfair font-semibold text-3xl mt-3">
-                Imaginative
-              </p>
-              <div className="w-1/2 md:w-3/4 h-32 bg-yellow absolute right-0 top-0 z-[-1]" />
+          <div className="flex flex-grow-0 gap-[44px]">
+            <div className="flex justify-center text-yellow font-semibold bg-[#010026] h-[24px]">
+              2022
+            </div>
+            <div className="flex flex-col">
+              <h2 className="text-md font-bold">Senior Product Designer</h2>
+              <p className="text-sm text-slate-300">Amazon Inc.</p>
             </div>
           </div>
-          <p className="mt-5">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
-            cumque veritatis perferendis sapiente sunt dicta veniam.
-          </p>
-        </motion.div>
+
+          <div className="flex gap-[44px]">
+            <div className="flex flex-none justify-center text-yellow font-semibold bg-[#010026] h-[24px]">
+              2020
+            </div>
+            <div className="flex flex-col gap-y-5">
+              <div>
+                <h2 className="text-md font-bold">
+                  Senior WP Frontend Developer
+                </h2>
+                <p className="text-sm text-slate-300">Envato</p>
+              </div>
+              <div>
+                <h2 className="text-md font-bold">
+                  Senior WP Frontend Developer
+                </h2>
+                <p className="text-sm text-slate-300">Envato</p>
+              </div>
+
+              <div>
+                <h2 className="text-md font-bold">
+                  Senior WP Frontend Developer
+                </h2>
+                <p className="text-sm text-slate-300">Envato</p>
+              </div>
+            </div>
+          </div>
+          <div className="flex gap-[44px]">
+            <div className="flex flex-none justify-center text-yellow font-semibold bg-[#010026] h-[24px]">
+              2022
+            </div>
+            <div className="flex flex-col">
+              <h2 className="text-md font-bold">Chief Executive Officer CEO</h2>
+              <p className="text-sm text-slate-300">Google</p>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
